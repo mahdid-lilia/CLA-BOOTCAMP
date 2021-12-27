141ab007daebd92240b91c92d3da2ebc377b8486
jl_mahdid@esi.dz <jl_mahdidesi.dz>
2021-12-27 02:03:14 +0100

    Project Submitted

--- Unit 1/Project/prep_unit_project-main/prep_unit_project-main/myenv2/Scripts/activate.bat
+++ Unit 1/Project/prep_unit_project-main/prep_unit_project-main/myenv2/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\My studies\CLA\CLA-BOOTCAMP\Unit 1\Project\prep_unit_project-main\prep_unit_project-main\myenv2
+set VIRTUAL_ENV=C:\MyStudies\CLA\CLA-BOOTCAMP\Unit 1\Project\prep_unit_project-main\prep_unit_project-main\myenv2
 
 if not defined PROMPT set PROMPT=$P$G
 
