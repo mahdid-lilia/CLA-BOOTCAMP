d47be0261549735fa41215b4308da24b3bf16fbb
jl_mahdid@esi.dz <jl_mahdidesi.dz>
2021-12-30 15:56:01 +0100

    Project Submitted

--- Unit 2/Project/data_analytics/Scripts/activate.bat
+++ Unit 2/Project/data_analytics/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\MyStudies\CLA\Unit2-Project\data_analytics
+set VIRTUAL_ENV=C:\MyStudies\CLA\CLA-BOOTCAMP\Unit 2\Project\data_analytics
 
 if not defined PROMPT set PROMPT=$P$G
 
